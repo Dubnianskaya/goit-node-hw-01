8a77711f2398f5de8affe32bae9c7cd9373ba101
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-05-22 01:34:07 +0300

    commands initiated

---
+++ index.js
@@ -0,0 +1,34 @@
+const contactsOperations = require("./contacts")
+// const argv = require('yargs').argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contacts = await contactsOperations.listContacts();
+      console.log(contacts)
+      break;
+
+    case 'get':
+        const contactById = await contactsOperations.getContactById(id);
+        if(!contactById) {
+            throw new Error(`Contact with id ${id} not found`)
+        }
+        console.log(contactById)
+        break;
+
+    case 'add':
+        const newContact = await contactsOperations.addContact(name, email, phone);
+        console.log(newContact)
+      break;
+
+    case 'remove':
+        const removedContact = await contactsOperations.removeContact(id);
+        console.log(removedContact)
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+// invokeAction(argv);
